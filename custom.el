759594ad8a2457a3a8317ec410c0d17055344dfe
erimendz-ubuntu2310 <erimendz@gmail.com>
2023-11-07 19:17:21 +0300

    add custom.el changes

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("3d9df5511048d0815b1ccc2204cc739117c1a458be92fb26c03451149a1b1c11" "4ca84f9861113f889d505b66c4a9c30b81400f450b54f4c857d3eb44c5bdc601" default))
  '(package-selected-packages
-   '(dracula-theme pod-mode muttrc-mode mutt-alias csv-mode yaml-mode wgrep vertico treemacs-all-the-icons tabbar spaceline-all-the-icons session org-roam org-modern orderless markdown-mode marginalia magit kind-icon json-mode initsplit imenu-list htmlize helm-org graphviz-dot-mode folding eproject embark-consult diminish corfu-terminal compile-multi-all-the-icons company color-theme-modern citar cape browse-kill-ring boxquote bm bar-cursor apache-mode all-the-icons-nerd-fonts all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer all-the-icons-gnus all-the-icons-dired)))
+   '(nov dracula-theme pod-mode muttrc-mode mutt-alias csv-mode yaml-mode wgrep vertico treemacs-all-the-icons tabbar spaceline-all-the-icons session org-roam org-modern orderless markdown-mode marginalia magit kind-icon json-mode initsplit imenu-list htmlize helm-org graphviz-dot-mode folding eproject embark-consult diminish corfu-terminal compile-multi-all-the-icons company color-theme-modern citar cape browse-kill-ring boxquote bm bar-cursor apache-mode all-the-icons-nerd-fonts all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer all-the-icons-gnus all-the-icons-dired)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
